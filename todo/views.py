3e377903c4306d680d44963f75d97e37034bf421
ebichiki <allegro.con.spirito28@gmail.com>
2018-12-20 23:52:40 +0900

    add user class

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,7 @@
-from flask import request, redirect, url_for, render_template, flash
+from functools import wraps
+from flask import request, redirect, url_for, render_template, flash, abort, jsonify, session, g
 from todo import app, db
-from todo.models import Entry
+from todo.models import Entry, User
 from datetime import datetime
 
 starttime = datetime.now()
@@ -14,6 +15,22 @@ def _jinja2_filter_datetime(entry):
 @app.template_filter('btnnm')
 def _jinja2_filter_datetime(arg):
     return status[arg]
+
+def login_required(f):
+    @wraps(f)
+    def decorated_view(*args, **kwargs):
+        if g.user is None:
+            return redirect(url_for('login', next=request.path))
+        return f(*args, **kwargs)
+    return decorated_view
+
+@app.before_request
+def load_user():
+    user_id = session.get('user_id')
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = User.query.get(session['user_id'])
 
 @app.route('/')
 def show_entries():
@@ -57,3 +74,74 @@ def del_entry(id):
     db.session.commit()
     flash('Delete a task!')
     return redirect(url_for('show_entries'))
+
+@app.route('/users/')
+@login_required
+def user_list():
+    users = User.query.all()
+    return render_template('user/list.html', users=users)
+
+@app.route('/users/<int:user_id>/')
+@login_required
+def user_detail(user_id):
+    user = User.query.get(user_id)
+    return render_template('user/detail.html', user=user)
+
+@app.route('/users/<int:user_id>/edit/', methods=['GET', 'POST'])
+@login_required
+def user_edit(user_id):
+    user = User.query.get(user_id)
+    if user is None:
+        abort(404)
+    if request.method == 'POST':
+        user.name = request.form['name']
+        user.email = request.form['email']
+        user.password = request.form['password']
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('user_detail', user_id=user_id))
+    return render_template('user/edit.html', user=user)
+
+@app.route('/users/create/', methods=['GET','POST'])
+@login_required
+def user_create():
+    if request.method == 'POST':
+        user = User(name=request.form['name'],
+                    email=request.form['email'],
+                    password=request.form['password'])
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('user_list'))
+    return render_template('user/edit.html')
+
+@app.route('/users/<int:user_id>/delete/', methods=['DELETE'])
+@login_required
+def user_delete(user_id):
+    user = User.query.get(user_id)
+    if user is None:
+        response = jsonify({'status': 'Not Found'})
+        response.status_code = 404
+        return response
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify({'status': 'OK'})
+
+@app.route('/login', methods={'GET', 'POST'})
+def login():
+    if request.method == 'POST':
+        user, authenticated = User.authenticate(db.session.query,
+                                                request.form['email'], request.form['password'])
+        if authenticated:
+            session['user_id'] = user.id
+            flash('login')
+            return redirect(url_for('show_entries'))
+        else:
+            flash('Invalid email or password')
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    flash('logout')
+    return redirect(url_for('show_entries'))
+
