3d96e218a19a8ef140237e9ef982594d3976b3e7
ebichiki <allegro.con.spirito28@gmail.com>
2018-12-21 21:47:56 +0900

    flask_migrate

--- manage.py
+++ manage.py
@@ -1,12 +1,18 @@
 from __future__ import print_function
 from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 from todo import app, db
 
-manager = Manager(app)
+# manager = Manager(app)
+#
+# @manager.command
+# def init_db():
+#     db.create_all()
 
-@manager.command
-def init_db():
-    db.create_all()
+migrate = Migrate(app, db)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
     manager.run()
--- todo/__init__.py
+++ todo/__init__.py
@@ -1,10 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import os
 
 app = Flask(__name__)
-#app.config.from_object('todo.config')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config.from_object('todo.config')
 db = SQLAlchemy(app)
 
 import todo.views
